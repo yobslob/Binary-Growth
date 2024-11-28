b625f8415aeac6220a9fa1c1036d8dc070869a53
yogesh <yogeshxiix@gmail.com>
2024-11-28 21:07:52 +0530

    added buttons

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -6,25 +6,25 @@ const Content = () => {
     return (
         <section
             id="content"
-            className="min-h-screen gradient-bg text-white flex flex-col items-center justify-center py-20 px-8"
+            className="min-h-screen gradient-bg text-white flex flex-col items-center justify-center"
         >
             {/* Buttons */}
-            <div className="flex space-x-8 mb-8">
+            <div className="flex space-x-36 mb-32">
                 <button
                     onClick={() => setActiveContent("short")}
-                    className={`px-6 py-3 rounded-lg font-bold text-lg ${activeContent === "short"
-                        ? "bg-blue-600 text-white"
-                        : "bg-gray-800 text-gray-400"
-                        } hover:bg-blue-500 transition-colors duration-300`}
+                    className={`px-16 py-2 rounded-xl text-md bord ${activeContent === "short"
+                        ? "bg-cyan-700 text-white"
+                        : "bg-black text-gray-400"
+                        } transform hover:-translate-y-1 transition-transform duration-300`}
                 >
                     Short Form Content
                 </button>
                 <button
                     onClick={() => setActiveContent("long")}
-                    className={`px-6 py-3 rounded-lg font-bold text-lg ${activeContent === "long"
-                        ? "bg-blue-600 text-white"
-                        : "bg-gray-800 text-gray-400"
-                        } hover:bg-blue-500 transition-colors duration-300`}
+                    className={`px-16 py-2 rounded-xl text-md bord ${activeContent === "long"
+                        ? "bg-cyan-700 text-white"
+                        : "bg-black text-gray-400"
+                        } transform hover:-translate-y-1 transition-transform duration-300`}
                 >
                     Long Form Content
                 </button>
@@ -32,20 +32,40 @@ const Content = () => {
 
             {/* Content */}
             <div className="text-center max-w-3xl">
-                {activeContent === "short" && (
-                    <p className="text-lg text-gray-300">
-                        Short form content is ideal for engaging quickly with your audience,
-                        offering concise and impactful messages that capture attention
-                        effortlessly.
+                {activeContent === "short" && (<>
+                    <p className="text-3xl mb-8 font-semibold">
+                        Our Shorts Will Outperform <span className="text-custPurple">Your Current Shorts</span><br></br>
+                        In Views by 10% Within 30 Days,<br></br>
+                        Or We Will Give You A Full Refund
                     </p>
-                )}
-                {activeContent === "long" && (
-                    <p className="text-lg text-gray-300">
-                        Long form content dives deeper into topics, providing in-depth
-                        information, detailed analysis, and storytelling that resonates on
-                        a profound level.
+                    <p className="text-md font-light">
+                        We will put this in a legally-binding contract with no BS or funny business
                     </p>
-                )}
+                    <div className="bord">
+                        <a
+                            href="#contact"
+                        >
+                            BOOK A CALL
+                        </a>
+                    </div>
+                </>)}
+                {activeContent === "long" && (<>
+                    <p className="text-3xl mb-12 font-semibold">
+                        Get your Channel's Watch Time<br></br>
+                        increased by 15%, With a <span className="text-custPurple">Youtube Lead Engine</span><br></br>
+                        (or You don't pay)
+                    </p>
+                    <p className="text-md font-light">
+                        We will put this in a legally-binding contract with no BS or funny business
+                    </p>
+                    <div className="bord">
+                        <a
+                            href="#contact"
+                        >
+                            BOOK A CALL
+                        </a>
+                    </div>
+                </>)}
             </div>
         </section>
     );
